adbe75522210f6cc8bb42bf5c7e8dc0d624fc229
rubens <ruben.schrurs@student.hogent.be>
2023-03-14 14:27:38 +0100

    [new] Icons and general style changes

--- src/components/home/Catalogue.jsx
+++ src/components/home/Catalogue.jsx
@@ -1,18 +1,45 @@
+import React, { useState } from 'react';
 import '../../css/home/Catalogue.scss'
 import CatalogueItem from './CatalogueItem'
 
 export default function Catalogue() {
+    const [showAllItems, setShowAllItems] = useState(false)
+
+    const handleShowMore = () => {
+      setShowAllItems(prevState => !prevState)
+    }
+
     return (
         <>
-            <div className="catalogue">
-                <h2>Catalogue</h2>
-                <div className='catalogueReleases'>
-                    <CatalogueItem/>
-                    <CatalogueItem/>
-                    <CatalogueItem/>
-                    <CatalogueItem/>
-                </div>
+          <div className="catalogue">
+            <h2>Catalogue</h2>
+            <div className="catalogueReleases">
+              {showAllItems ? (
+                <>
+                  <CatalogueItem />
+                  <CatalogueItem />
+                  <CatalogueItem />
+                  <CatalogueItem />
+                  <CatalogueItem />
+                  <CatalogueItem />
+                  <CatalogueItem />
+                  <CatalogueItem />
+                </>
+              ) : (
+                <>
+                  <CatalogueItem />
+                  <CatalogueItem />
+                  <CatalogueItem />
+                  <CatalogueItem />
+                </>
+              )}
             </div>
+            <div className='showMoreDiv'>
+                <button onClick={handleShowMore} className="showMoreButton">
+                {showAllItems ? 'Show Less' : 'Show More'}
+                </button>
+            </div>
+          </div>
         </>
-    )
+      )
 }
--- src/components/home/Filters.jsx
+++ src/components/home/Filters.jsx
@@ -1,27 +1,41 @@
 import '../../css/home/Filters.scss'
-import { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useReleases } from '../../contexts/ReleaseProvider'
 import Filter from './Filter'
 
 export default function Filters() {
     const { genres, getAllGenres } = useReleases()
+    const [showAllGenres, setShowAllGenres] = useState(false)
 
-    // Add dependency causes infinite loop
+    // Adding dependency causes infinite loop
     useEffect(() => {
         try {
           getAllGenres()
         } catch (error) {
           console.error(error)
         }
-      }, [])
+    }, [])
     
+    const handleShowMore = () => {
+      setShowAllGenres(prevState => !prevState); // toggle the state of showAllGenres
+    }
+
     return (
-        <>
-            <div className="filters">
-                {  genres?.map((genre) =>
-                  <Filter genreName={genre} key={Math.random() * 10}/>
-                )}
-            </div>
-        </>
+      <>
+        <div className="filterWrapper">
+          <div className='filters'>
+            {showAllGenres
+              ? genres?.map(genre => <Filter genreName={genre} key={genre} />)
+              : genres?.slice(0, 6).map(genre => <Filter genreName={genre} key={genre} />)}
+          </div>
+          <div className='showMoreDiv'>
+            {genres && genres.length > 5 && (
+              <button onClick={handleShowMore} className="showMoreButton">
+                {showAllGenres ? 'Show Less' : 'Show More'}
+              </button>
+            )}
+          </div>
+        </div>
+      </>
     )
 }

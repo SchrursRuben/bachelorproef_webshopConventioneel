004637c9193eac5a5cd441d354b87e252fe22a33
rubens <ruben.schrurs@student.hogent.be>
2023-05-19 18:09:41 +0200

    [upd] vercel

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import {BrowserRouter as Router} from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 
 import App from "./App";
